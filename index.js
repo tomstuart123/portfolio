3123e1c9ca18dadbe5933229a913fc4cff73c4ea
Tommy <tomstuart123@hotmail.co.uk>
2019-12-15 11:42:44 -0500

    illustrations added

---
+++ index.js
@@ -0,0 +1,54 @@
+// TO DO
+// 1: Add social links
+// 2: Make portfolio images nicer
+// 3. Work on progress bars
+// 4. Add animations to site
+// 5. Add Resume page
+// 6. Add about me page
+
+
+const portfolioApp = {};
+
+portfolioApp.pageLoad = function () {
+    console.log('pageLoaded')
+}
+
+portfolioApp.events = function () {
+    // in skills section, toggle the developer skillset for the product skill set on click
+    $('.skills__sliderHeaders').on('click', '.skills__subHeader', function (event) {
+        // toggle the titles first
+        $('.skills__subHeaderOne').toggleClass('skills__subHeader--active');
+        $('.skills__subHeaderOne').toggleClass('skills__subHeader');
+        // then toggle the skills themselves and progress bars
+        $('.skills__sliderContainer').toggleClass('visuallyHidden');
+        $('.skills__imageContainer').toggleClass('visuallyHidden');
+    })
+
+    // in portfolio section, toggle the developer portfolio for the product one on click
+    $('.portfolio__list').on('click', '.portfolio__item--inactive', function (event) {
+        // toggle the titles first
+        $('.portfolio__item').toggleClass('portfolio__item--inactive');
+        $('.portfolio__item').toggleClass('portfolio__item--active');
+        // toggle the height to accommodate the changing portfolio
+        $('.portfolio').toggleClass('portfolio--active');
+        // then toggle the portfolio itself 
+        $('.portfolio__galleryItem').toggleClass('visuallyHidden');
+    })
+
+    // scroll to contact page
+    $('.landing__button').on('click', function (event) {
+        window.scrollBy(0, 5000);
+    })
+
+}
+
+
+portfolioApp.init = function () {
+    portfolioApp.events();
+    portfolioApp.pageLoad();
+}
+
+// run init in jquery
+$(function () {
+    portfolioApp.init();
+});
