f69c25068ce50bc3d00dd02ad5b5081fbae6ba93
Tommy <tomstuart123@hotmail.co.uk>
2019-12-15 17:40:02 -0500

    made responsive

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TO DO
 // 1: Add social links
 // 2: Make portfolio images nicer
-// 3. Work on progress bars
+// 3. Work on progress bars colurs (and appear in mobile)
 // 4. Add animations to site
 // 5. Add Resume page
 // 6. Add about me page
