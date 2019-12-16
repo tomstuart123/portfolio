bfc4b65fee1140be1311290f6dc1865ab1618126
Tommy <tomstuart123@hotmail.co.uk>
2019-12-16 16:01:50 -0500

    need to change illustrations

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // TO DO
-// 1: Make portfolio images nicer
-// 2. Work on progress bars colurs (and appear in mobile)
-// 3. Add haircuts
-// 4. Add animations to site
+// 2. Add haircuts
+// 3. Make hover portfolio and progress bars in mobile
+// 4. Animate portfolio / skills section
 // 5. Add Resume page
 // 6. Add about me page
 
